31170b0256a6578bc4111d8c9281cafd1f267636
Pedro <goodgeneral885@gmail.com>
2024-11-01 21:42:04 -0300

    Project Created

---
+++ src/test/java/com/tads/catalogo/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.catalogo.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
