403f1e3d68c09a1fd63c4a86e7b86c6f850572d6
Pedro <goodgeneral885@gmail.com>
2024-11-01 23:21:21 -0300

    Aula 1 primeiras funcoes

---
+++ src/main/java/com/tads/catalogo/catalogo/resources/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.tads.catalogo.catalogo.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tads.catalogo.catalogo.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@GetMapping
+	public ResponseEntity <List < Category > > findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L,"Book"));
+		list.add(new Category(2L,"Eletronics"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+
+}
